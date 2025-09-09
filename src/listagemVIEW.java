afe2ea599c4372bc928288735f666c720955cc93
PatriciaEfrom <patriciaefrom@gmail.com>
2025-09-08 21:37:40 -0300

    Implementando botão de Vendido.

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -136,12 +137,23 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        String id = id_produto_venda.getText();
+        String idText = id_produto_venda.getText();
         
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        listarProdutos();
+        if(idText.isEmpty()){
+            JOptionPane.showMessageDialog(this, "Digite o ID do Produto!");
+            return;
+            }
+            
+            try{
+            int idProduto = Integer.parseInt(idText);
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.venderProduto(idProduto);
+            JOptionPane.showMessageDialog(this,  "Produto vendido com sucesso!");
+            listarProdutos();
+            id_produto_venda.setText("");
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(this, "ID inválido! Digite apenas números.");
+        }
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
